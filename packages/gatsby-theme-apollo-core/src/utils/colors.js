f11952583c00fd599e42dff0a037afca3855e957
Pascal Kaufmann <kaufmae@me.com>
2020-08-19 13:21:24 +0200

    core

--- packages/gatsby-theme-apollo-core/src/utils/colors.js
+++ packages/gatsby-theme-apollo-core/src/utils/colors.js
@@ -1,11 +1,11 @@
 const {colors} = require('@apollo/space-kit/colors');
 
 exports.colors = {
-  primary: '#17fac5',
-  primaryLight: '#d1fef3',
-  secondary: '#1f5bb4',
-  tertiary: '#1d3335',
-  tertiaryLight: '#e9edf3',
+  primary: '#1f5bb4',
+  primaryLight: '#7fa9e9',
+  secondary: '#1d3335',
+  tertiary: '#17fac5',
+  tertiaryLight: '#d1fef3',
   divider: colors.silver.dark,
   background: colors.silver.light,
   background2: colors.silver.base,
