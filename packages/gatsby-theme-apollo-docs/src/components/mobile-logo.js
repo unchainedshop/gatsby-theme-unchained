a0514fb2950317772716609ced7bc558abdae17e
Mikael Araya <mikaelaraya12@gmail.com>
2021-08-25 17:58:00 +0300

    Change new logo #87

--- packages/gatsby-theme-apollo-docs/src/components/mobile-logo.js
+++ packages/gatsby-theme-apollo-docs/src/components/mobile-logo.js
@@ -1,34 +1,33 @@
 import React from 'react';
 
 export const MobileLogo = ({width}) => (
-  <svg viewBox="0 0 620 680" width={width} xmlns="http://www.w3.org/2000/svg">
-    <g fill="none">
-      <path
-        d="m177.1 248.2c-9.422226-17.65486-9.422226-38.84514 0-56.5l-47.1 28.3z"
-        fill="#6065f3"
-      />
-      <path
-        d="m14.7 174.8c-6.20876155 29.813515-6.20876155 60.586485 0 90.4l75.3-45.2z"
-        fill="#6065f3"
-      />
-      <path d="m230 360v140l-160-100z" fill="#00f5bc" />
-      <g fill="#6065f3">
-        <path d="m130 280h-111.7c12.810773 45.098728 39.6812724 84.931528 76.7 113.7l35-8.7z" />
-        <path d="m390 320v140l-160-100z" />
-        <path d="m230 280 91.4 57.1 68.6-17.1v-40z" />
-        <path d="m230 500v140l-160-100z" />
-        <path d="m390 460v140l-160-100z" />
-        <path d="m390 600v80l-160-40z" />
-        <path d="m230 640v40h-160z" />
-        <path d="m0 470c0 38.659932 31.3400675 70 70 70v-140c-38.6599325 0-70 31.340068-70 70z" />
-        <path d="m0 610c0 38.659932 31.3400675 70 70 70v-140c-38.6599325 0-70 31.340068-70 70z" />
-        <path d="m550.000001 280-160.000001 40 160.000001 100c38.659932-.000001 69.999999-31.340068 69.999999-70s-31.340067-69.999999-69.999999-70z" />
-        <path d="m550 420-160 40 160 100c38.659932 0 70-31.340068 70-70s-31.340068-70-70-70z" />
-      </g>
-      <g fill="#00f5bc" transform="translate(18)">
-        <path d="m532 560-160 40 160 80c33.137085 0 60-26.862915 60-60s-26.862915-60-60-60z" />
-        <path d="m432 100-60 60h-371.7c26.1-92.3 111-160 211.7-160 122.9 0 220 100 220 100zm-170-60c-16.568542 0-30 13.4314575-30 30s13.431458 30 30 30 30-13.4314575 30-30-13.431458-30-30-30z" />
-      </g>
+  <svg width="76" height="76" viewBox="0 0 76 76" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0)">
+      <path d="M49.4284 62.8043L42.4211 66.664C41.4536 67.1969 40.2372 66.8446 39.7043 65.8771L26.1954 41.3514C22.7927 35.1736 24.5458 27.5466 30.0204 23.4232L50.2153 60.0876C50.7482 61.0551 50.3959 62.2714 49.4284 62.8043Z" stroke="url(#paint0_linear)" stroke-width="4"/>
+      <path d="M70.4504 51.2253L63.4431 55.085C62.4756 55.6179 61.2592 55.2656 60.7263 54.2981L40.5314 17.6337C46.9423 15.2105 54.3257 17.8051 57.7284 23.9828L71.2373 48.5085C71.7702 49.4761 71.4179 50.6924 70.4504 51.2253Z" stroke="url(#paint1_linear)" stroke-width="4"/>
+      <path d="M50.1122 35.0279C53.515 41.2057 51.7618 48.8327 46.2872 52.9561L26.0923 16.2917C25.5594 15.3242 25.9117 14.1079 26.8792 13.575L33.8865 9.71527C34.8541 9.18236 36.0704 9.53468 36.6033 10.5022L50.1122 35.0279Z" stroke="url(#paint2_linear)" stroke-width="4"/>
+      <path d="M18.5792 52.3965L4.3922 26.6398C4.23377 26.3521 4.33851 25.9905 4.62615 25.8321L12.8645 21.2944C13.832 20.7614 15.0484 21.1138 15.5813 22.0813L35.7762 58.7456C29.3653 61.1689 21.9819 58.5743 18.5792 52.3965Z" stroke="url(#paint3_linear)" stroke-width="4"/>
     </g>
+    <defs>
+      <linearGradient id="paint0_linear" x1="46.8897" y1="66.486" x2="23.7316" y2="24.442" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#041626"/>
+        <stop offset="1" stop-color="#0A1C2B" stop-opacity="0"/>
+      </linearGradient>
+      <linearGradient id="paint1_linear" x1="67.9117" y1="54.907" x2="44.7536" y2="12.8629" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#041626"/>
+        <stop offset="1" stop-color="#071A2B" stop-opacity="0"/>
+      </linearGradient>
+      <linearGradient id="paint2_linear" x1="52.5761" y1="51.9373" x2="29.418" y2="9.89328" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#0B1E2F" stop-opacity="0"/>
+        <stop offset="1" stop-color="#041626"/>
+      </linearGradient>
+      <linearGradient id="paint3_linear" x1="31.554" y1="63.5164" x2="8.39592" y2="21.4724" gradientUnits="userSpaceOnUse">
+        <stop stop-color="#041626" stop-opacity="0"/>
+        <stop offset="1" stop-color="#041626"/>
+      </linearGradient>
+      <clipPath id="clip0">
+        <rect width="76" height="76" fill="white"/>
+      </clipPath>
+    </defs>
   </svg>
 );
